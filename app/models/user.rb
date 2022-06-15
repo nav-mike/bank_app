999fa498d68b70c9f579eb48f6b6f9ecdbe77863
Mikhail Navrotskii <m.navrotskiy@gmail.com>
2022-06-15 18:23:59 +0300

    update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  act_as_authentic
+
+  validates :email, presence: true, uniqueness: { case_sensitive: false },
+                    format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: 'should look like an email address.' },
+                    length: { maximum: 100 }
+
+  validates :password, confirmation: { if: :require_password? }, length: { minimum: 8, if: :require_password? }
+  validates :password_confirmation, length: { minimum: 8, if: :require_password? }
 end
