b8a048577baf760146d244d7e1ef152d1987ebb6
Mikhail Navrotskii <m.navrotskiy@gmail.com>
2022-06-19 16:23:24 +0300

    add user dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user_session, :current_user
 
+  before_action :require_user
+
   protected
 
   def handle_unverified_request
@@ -19,5 +21,12 @@ class ApplicationController < ActionController::Base
     return @current_user if defined?(@current_user)
 
     @current_user = current_user_session&.user
+  end
+
+  def require_user
+    unless current_user
+      redirect_to new_user_session_url
+      false
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
 
   resources :users
   resource :user_session
+
+  root 'users#show'
 end
