c7aa50343754a3fe2dccb16df8b4e6893c77cdec
Mikhail Navrotskii <m.navrotskiy@gmail.com>
2022-06-19 16:47:28 +0300

    add basic dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
   def show
+    @user = User.includes(:accounts).find(current_user.id)
+    @incomes = MoneyTransfer.where(receiver: @user.accounts).includes(sender: :user).order(created_at: :desc)
+    @outcomes = MoneyTransfer.where(sender: @user.accounts).includes(receiver: :user).order(created_at: :desc)
   end
 end
