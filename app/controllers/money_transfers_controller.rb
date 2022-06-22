9cf2b6187c0446165fc46789acadf48e65629bfe
Mikhail Navrotskii <m.navrotskiy@gmail.com>
2022-06-22 17:45:23 +0300

    add creation action

--- app/controllers/money_transfers_controller.rb
+++ app/controllers/money_transfers_controller.rb
@@ -3,4 +3,12 @@ class MoneyTransfersController < ApplicationController
     @money_transfer = MoneyTransfer.new
     @users = User.where.not(id: current_user.id).not_bank.map { [_1.email, _1.id] }
   end
+
+  def create
+    receiver = User.find(money_transfer_params[:receiver_id])
+    MoneyTransfers::ToUser.call(current_user.accounts.first.id, receiver.accounts.first&.id, money_transfer_params[:amount].to_i)
+    redirect_to root_path
+  end
+
+  def money_transfer_params = params.require(:money_transfer).permit(:amount, :receiver_id)
 end
